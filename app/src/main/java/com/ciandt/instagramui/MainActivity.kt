564fdbcd0a968623d9e31e1bd53a5b199c660895
Wilkison Silva <wilkisons@ciandt.com>
2022-09-22 15:26:36 -0400

    feat: cria topbar

---
+++ app/src/main/java/com/ciandt/instagramui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.ciandt.instagramui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ciandt.instagramui.ui.ProfileScreen
+import com.ciandt.instagramui.ui.theme.InstagramUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProfileScreen()
+        }
+    }
+}
+
